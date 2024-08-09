b2dff1e3e79d15f27019609260367266b80f6414
=Raju9934 <raju.gupta10u@gmail.com>
2024-08-09 10:23:58 +0530

    given color to home page

--- src/Home.js
+++ src/Home.js
@@ -2,10 +2,35 @@ import './Home.css'
 function Home()
 {
     return(
-<div>
+        <>
+<div className='d1'>
     <h1>This is  a heading page</h1>
     <h2>Hello Everyone I am Going to Build my Project</h2>
 </div>
+<div className='d2'>
+BOX1
+</div>
+<div className='d3'>
+BOX2
+</div>
+<div className='d4'>
+BOX3
+</div>
+<div className='d5'>
+BOX4
+</div>
+<div className='d6'>
+BOX5
+</div>
+<div className='d7'>
+BOX6
+</div>
+<div className='d8'>
+BOX7
+</div>
+        
+        </>
+
     );
 }
 export default Home;
